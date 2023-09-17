6f6971c6f342f4ed08007446124b9ebb515bfdd1
souhailelhayani <souhail.200308@gmail.com>
2023-09-16 21:42:21 -0400

    first commit

---
+++ src/main/java/com/spring_peerfit_project/peerfit/PeerfitApplication.java
@@ -0,0 +1,13 @@
+package com.spring_peerfit_project.peerfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring_peerfit_project/peerfit/PeerfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_peerfit_project.peerfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
